9af77315728cf570fdac8920b7b7c809baad0ca0
petedigipulse <peter@digitalpulse.co.nz>
2015-07-13 15:42:22 +1200

    Add helper for text_field

--- my_rails_forms/lib/form_builder.rb
+++ my_rails_forms/lib/form_builder.rb
@@ -5,4 +5,14 @@ class PetesFormBuilder < ActionView::Helpers::FormBuilder
 	include ActionView::Helpers::TextHelper
 
 	attr_accessor :output_buffer
+
+	def text_field(attributes, options={})
+		content_tag(:div, class: "row") do
+			content_tag(:div, class: "col-sm-12") do 
+				super(attributes, options)
+			end
+		end
+	end
+
+
 end
